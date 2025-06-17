2d2fef1010d04be42f8f2d842d74cbca0ecebadf
Alex <alex@example.com>
2025-06-17 21:03:44 +0300

    Переделанный 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,12 +47,6 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-//    implementation(libs.androidx.activity.compose)
-//    implementation(platform(libs.androidx.compose.bom))
-//    implementation(libs.androidx.ui)
-//    implementation(libs.androidx.ui.graphics)
-//    implementation(libs.androidx.ui.tooling.preview)
-//    implementation(libs.androidx.material3)
     implementation(libs.androidx.recyclerview)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.navigation.fragment.ktx)
@@ -63,18 +57,11 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-//    implementation ("io.coil-kt:coil-compose:2.4.0")
-
 
     implementation ("androidx.core:core-ktx:1.12.0")
     implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-//    implementation ("androidx.activity:activity-compose:1.7.2")
-//    implementation ("androidx.compose.ui:ui:1.5.0")
-//    implementation ("androidx.compose.material3:material3:1.2.0")
-//    implementation ("androidx.navigation:navigation-compose:2.7.0")
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-//    implementation("androidx.compose.material:material-icons-extended:1.5.0")
 
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
@@ -93,7 +80,6 @@ dependencies {
     implementation ("com.google.android.material:material:1.9.0")
     implementation ("androidx.navigation:navigation-fragment-ktx:2.6.0")
     implementation ("androidx.navigation:navigation-ui-ktx:2.6.0")
-
 
     kapt ("androidx.hilt:hilt-compiler:1.0.0")
     implementation("androidx.hilt:hilt-navigation-fragment:1.0.0")
